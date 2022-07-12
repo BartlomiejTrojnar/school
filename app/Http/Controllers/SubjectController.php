bf1d22d3db85cfb488ccc28db677a7307fd227a4
BartlomiejTrojnar <bartek_tr@tlen.pl>
2022-07-12 13:52:56 +0200

    2022-07-12 Modernizacja widoków dla przedmiotu

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 use App\Models\Subject;
 use App\Repositories\SubjectRepository;
 
-use App\Models\ExamDescription;
-use App\Models\SchoolYear;
-use App\Models\Session;
 use App\Models\TaughtSubject;
 use App\Repositories\ExamDescriptionRepository;
 use App\Repositories\GradeRepository;
@@ -65,7 +62,7 @@ class SubjectController extends Controller
 
     public function change($id) {  session()->put('subjectSelected', $id);   }
 
-    public function show($id, SubjectRepository $subjectRepo, SchoolYearRepository $syR, GradeRepository $gradeRepo, TeacherRepository $tR, GroupRepository $groupRepo, $view='') {
+    public function show($id, SubjectRepository $subjectRepo, SchoolYearRepository $syR, GradeRepository $gradeRepo, TeacherRepository $tR, GroupRepository $groupRepo, SessionRepository $sessionRepo, ExamDescriptionRepository $edR, $view='') {
         if(empty(session()->get('subjectView')) || session()->get('subjectView')=='change')  session()->put('subjectView', 'showInfo');
         if($view)  session()->put('subjectView', $view);
         session() -> put('subjectSelected', $id);
@@ -75,11 +72,11 @@ class SubjectController extends Controller
         list($this->previous, $this->next) = $subjectRepo -> nextAndPreviousRecordId($subjects, $id);
 
         switch(session()->get('subjectView')) {
-            case 'showInfo':   return $this -> showInfo($this->subject);
-            case 'showTeachers': return $this -> showTeachers($this->subject);
-            case 'showGroups':   return $this -> showGroups($subjectRepo, $syR, $gradeRepo, $tR, $groupRepo);
-            case 'showExamDescriptions':   return $this -> showExamDescriptions($subject);
-            case 'showTextbooks':   return $this -> showTextbooks($subject);
+            case 'info':            return $this -> showInfo($this->subject);
+            case 'nauczyciele':     return $this -> showTeachers($this->subject, $syR);
+            case 'grupy':           return $this -> showGroups($syR, $gradeRepo, $tR, $groupRepo);
+            case 'opisy-egzaminow': return $this -> showExamDescriptions($this->subject, $sessionRepo, $edR);
+            case 'podreczniki':     return $this -> showTextbooks($this->subject);
             default:
                 printf('<p style="background: #bb0; color: #f00; font-size: x-large; text-align: center; border: 3px solid red; padding: 5px;">Widok %s nieznany</p>', session()->get('subjectView'));
         }
@@ -90,8 +87,7 @@ class SubjectController extends Controller
             -> nest('subView', 'subject.showInfo', ["subject"=>$subject]);
     }
 
-    private function showTeachers($subject) {
-        $schoolYearRepo = new SchoolYearRepository(new SchoolYear);
+    private function showTeachers($subject, $schoolYearRepo) {
         $schoolYears = $schoolYearRepo->getAllSorted();
         $subjectTeachers = $subject -> teachers;
         $schoolYear_id = session() -> get('schoolYearSelected');
@@ -102,17 +98,17 @@ class SubjectController extends Controller
                 unset($subjectTeachers[$i]);
         }
         $unlearningTeachers = TaughtSubject::unlearningTeachers($subjectTeachers);
-        $schoolYearSelectField = view('schoolYear.selectField', ["schoolYears"=>$schoolYears, "schoolYearSelected"=>session()->get('schoolYearSelected'), "name"=>"schoolYear_id" ]);
-        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next])
-            -> nest('subView', 'subject.showTeachers', ["subject"=>$subject, "subjectTeachers"=>$subjectTeachers, "unlearningTeachers"=>$unlearningTeachers, "schoolYearSelectField"=>$schoolYearSelectField]);
+        $schoolYearSF = view('schoolYear.selectField', ["schoolYears"=>$schoolYears, "schoolYearSelected"=>session()->get('schoolYearSelected'), "name"=>"schoolYear_id" ]);
+        $subjectTeachersView = view('subject.showTeachers', ["subject"=>$subject, "subjectTeachers"=>$subjectTeachers, "unlearningTeachers"=>$unlearningTeachers, "schoolYearSF"=>$schoolYearSF]);
+        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next, "subView"=>$subjectTeachersView]);
     }
 
-    private function showGroups($subjectRepo, $syRepo, $gradeRepo, $teacherRepo, $groupRepo) {
+    private function showGroups($syRepo, $gradeRepo, $teacherRepo, $groupRepo) {
         $gradeSelected = session()->get('gradeSelected');
         $levelSelected = session()->get('levelSelected');
-        $dateStart = session() -> get('dateView');
-        if(!empty(session() -> get('dateEnd'))) $dateEnd = session() -> get('dateEnd'); else $dateEnd=$dateStart;
-        $groups = $groupRepo -> getFilteredAndSorted($gradeSelected, $this->subject->id, $levelSelected, $dateStart, $dateEnd);
+        $start = session() -> get('dateView');
+        if(!empty(session() -> get('dateEnd'))) $end = session() -> get('dateEnd'); else $end=$start;
+        $groups = $groupRepo -> getFilteredAndSorted($gradeSelected, $this->subject->id, $levelSelected, $start, $end);
         $schoolYear = session()->get('schoolYearSelected');
         if( !empty($schoolYear) )  $schoolYear = $syRepo -> find( $schoolYear );
         $grades = $gradeRepo -> getFilteredAndSorted(substr($schoolYear->date_end, 0, 4), 0);
@@ -122,44 +118,37 @@ class SubjectController extends Controller
         $teachers = $teacherRepo -> getAll();
         $teacherSelected = session()->get('teacherSelected');
         $teacherSelectField = view('teacher.selectField', ["teachers"=>$teachers, "teacherSelected"=>$teacherSelected]);
-        $groupTable = view('group.table', ["subTitle"=>"grupy przedmiotu", "subject"=>$this->subject, "groups"=>$groups, "link"=>true, "dateStart"=>$dateStart, "dateEnd"=>$dateEnd,
-            "grade_id"=>$gradeSelected, "gradeSelectField"=>$gradeSelectField, "subjectSelectField"=>"", "levelSelectField"=>$levelSelectField, "teacherSelectField"=>$teacherSelectField]);
+        $groupTable = view('group.table', ["subTitle"=>"grupy przedmiotu", "groups"=>$groups, "link"=>true, "start"=>$start, "end"=>$end,
+            "grade_id"=>$gradeSelected, "gradeSelectField"=>$gradeSelectField, "subjectSelectField"=>"",
+            "levelSelectField"=>$levelSelectField, "teacherSelectField"=>$teacherSelectField, "schoolYearSelectField"=>""]);
         return view('subject.show', ["subject"=>$this->subject, "previous"=>$this->previous, "next"=>$this->next, "subView"=>$groupTable]);
     }
 
-    private function showExamDescriptions($subject) {
-        $sessionRepo = new SessionRepository(new Session);
-        $examDescriptionRepo = new ExamDescriptionRepository(new ExamDescription);
+    private function showExamDescriptions($subject, $sessionRepo, $examDescriptionRepo) {
         $sessions = $sessionRepo -> getAllSorted();
         $sessionSelected = session()->get('sessionSelected');
-        $sessionSelectField = view('session.selectField', ["sessions"=>$sessions, "sessionSelected"=>$sessionSelected]);
+        $sessionSF = view('session.selectField', ["sessions"=>$sessions, "sessionSelected"=>$sessionSelected]);
 
         $examTypes = array('pisemny', 'ustny');
         $examTypeSelected = session()->get('examTypeSelected');;
-        $examTypeSelectField = view('examDescription.examTypeSelectField', ["examTypes"=>$examTypes, "examTypeSelected"=>$examTypeSelected]);
+        $examTypeSF = view('examDescription.examTypeSelectField', ["examTypes"=>$examTypes, "examTypeSelected"=>$examTypeSelected]);
 
         $levels = array('rozszerzony', 'podstawowy', 'nieustalony');
         $levelSelected = session()->get('levelSelected');;
-        $levelSelectField = view('layouts.levelSelectField', ["levels"=>$levels, "levelSelected"=>$levelSelected]);
+        $levelSF = view('layouts.levelSelectField', ["levels"=>$levels, "levelSelected"=>$levelSelected]);
 
         $examDescriptions = $examDescriptionRepo -> getFilteredAndSorted($subject->id, $sessionSelected, $examTypeSelected, $levelSelected);
+        $countDesc = count($examDescriptions);
+        $examDescriptionsTable = view('examDescription.tableForSubject', ["countDesc"=>$countDesc, "sessionSF"=>$sessionSF, "examTypeSF"=>$examTypeSF, "levelSF"=>$levelSF, "examDescriptions"=>$examDescriptions]);
 
-        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next])
-            -> nest('subView', 'examDescription.table', ["examDescriptions"=>$examDescriptions, "subTitle"=>"opisy egzaminów",
-                "sessionSelectField"=>$sessionSelectField, "subjectSelectField"=>"",
-                "examTypeSelectField"=>$examTypeSelectField, "levelSelectField"=>$levelSelectField]);
+        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next, "subView"=>$examDescriptionsTable]);
     }
 
     private function showTextbooks($subject) {
-        $subjectRepo = new SubjectRepository(new Subject);
-        $subjects = $subjectRepo -> getActualAndSorted();
-        $subjectSelected = session()->get('subjectSelected');
-        $subjectSelectField = view('subject.selectField', ["subjects"=>$subjects, "subjectSelected"=>$subjectSelected]);
-
         $subTitle = "podręczniki dla przedmiotu";
         $textbooks = $subject -> textbooks;
-        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next])
-            -> nest('subView', 'textbook.table', ["subTitle"=>$subTitle, "textbooks"=>$textbooks, "subjectSelectField"=>$subjectSelectField]);
+        $textbookTable = view('textbook.tableForSubject', ["subTitle"=>$subTitle, "textbooks"=>$textbooks]);
+        return view('subject.show', ["subject"=>$subject, "previous"=>$this->previous, "next"=>$this->next, "subView"=>$textbookTable]);
     }
 
     public function edit(Request $request, Subject $subject) {
--- resources/views/subject/show.blade.php
+++ resources/views/subject/show.blade.php
@@ -16,11 +16,11 @@
 
 @section('main-content')
   <ul class="nav nav-tabs nav-justified">
-    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/showInfo') }}">informacje <i class='fas fa-info-circle'></i></a></li>
-    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/showTeachers') }}">nauczyciele <i class='fas fa-chalkboard-teacher'></i></a></li>
-    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/showGroups') }}">grupy <i class='fas fa-users'></i></a></li>
-    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/showExamDescriptions') }}">opisy egzaminów <i class="fas fa-atlas"></i></a></li>
-    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/showTextbooks') }}">podręczniki <i class='fas fa-book'></i></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/info') }}">informacje <i class='fas fa-info-circle'></i></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/nauczyciele') }}">nauczyciele <i class='fas fa-chalkboard-teacher'></i></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/grupy') }}">grupy <i class='fas fa-users'></i></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/opisy-egzaminow') }}">opisy egzaminów <i class="fas fa-atlas"></i></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('przedmiot/'.$subject->id.'/podreczniki') }}">podręczniki <i class='fas fa-book'></i></a></li>
     <li class="nav-item"><a class="nav-link" href="{{ route('przedmiot.index') }}">powrót <i class='fa fa-undo'></i></a></li>
   </ul>
 
