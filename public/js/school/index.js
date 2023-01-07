24bd72a2d97dc86618305113026b181585ce8a40
BartlomiejTrojnar <bartek_tr@tlen.pl>
2023-01-07 22:19:21 +0100

    2022-01-07 Modernizacja skryptu school/index.js sla widoku szkola/index

--- public/js/school/index.js
+++ public/js/school/index.js
@@ -1,215 +1,130 @@
-// ------------------------ (C) mgr inż. Bartłomiej Trojnar; 31.12.2022 ------------------------ //
-// ------------------------- wydarzenia na stronie wyświetlania szkół -------------------------- //
-var fadeOutTime = 575, fadeInTime = 1275;
+// ------------------------ (C) mgr inż. Bartłomiej Trojnar; 07.01.2023 ------------------------ //
+// -------------------------- wydarzenia dla widoku przedmiot/index  --------------------------- //
+const FADE_OUT=575, FADE_IN=5275, SLIDE_UP=1250, SLIDE_DOWN=1250;
+const NUMBER_OF_FIELDS=4, TABLE_NAME="#schools", DATA_NAME="school_id", INPUT_NAME="name", ROUTE_NAME="szkola";
 
-function refreshRow(id, lp, add="true") {  // odświeżenie wiersza z informajami o szkole o podanym identyfikatorze
+import '../patternForIndex.js';
+import { CreateRowService, RefreshRowService } from '../patternForIndex.js';
+import { EditRowService } from '../patternForIndex.js';
+
+// ------------------- odświeżanie wiersza tabeli z informacjami o rekordzie ------------------- //
+function refreshRow(id, lp, operation="add", success="true") {   // odświeżenie wiersza z informajami o rekordzie o podanym identyfikatorze
+    var RefreshRow = new RefreshRowService(NUMBER_OF_FIELDS, TABLE_NAME, DATA_NAME);
+    if(!success)
+        switch(operation) {
+            case "update": RefreshRow.updateError(id, "Błąd w trakcie modyfikowania szkoły."); break;
+            case "add": RefreshRow.addError("Błąd w trakcie dodawania szkoły."); return;
+            case "destroy": RefreshRow.destroyError(id, "Nie można usunąć szkoły. Prawdopodobnie istnieją powiązane rekordy."); return;
+        }
+    if(operation=="destroy")        { RefreshRow.destroySuccess(id); return; }
+
     $.ajax({
         method: "POST",
         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-        url: "http://localhost/school/szkola/refreshRow",
+        url: "http://localhost/school/" +ROUTE_NAME+ "/refreshRow",
         data: { id: id, lp: lp },
-        success: function(result) { refreshRowSuccess(id, result, add); },
-        error: function() { refreshRowError(id, add); },
+        success: function(result) { RefreshRow.success(id, result, operation); },
+        error: function() { RefreshRow.error(id, operation, "Błąd odświeżania wiersza z szkoły."); },
     });
 }
 
-function refreshRowError(id, add) {
-    var error = '<tr data-school_id="'+id+'"><td class="error" colspan="4">Błąd odświeżania wiersza z informacjami o szkole.</td></tr>';
-    if(add) $('tr.create').before(error);
-    else $('tr[data-school_id="'+id+'"]').replaceWith(error);
-    $('td.error').hide().fadeIn(fadeInTime);
-}
-
-function refreshRowSuccess(id, result, add) {
-    if(add) { refreshRowSuccessForAdd(result); }
-    else    { refreshRowSuccessForEdit(id, result); }
-}
-
-function refreshRowSuccessForAdd(result) {
-    alert('refreshSuccess Add');
-    $('tr.create').before(result);
-    $('tr[data-school_id="'+id+'"]').fadeIn(fadeInTime);
-    $('#showCreateRow').show();
-}
-
-function refreshRowSuccessForEdit(id, result) {
-    $.when( $('tr[data-school_id="'+id+'"]').fadeOut(fadeOutTime) ).then(function() {
-        $('tr[data-school_id="'+id+'"]').replaceWith(result);
-        $('tr[data-school_id="'+id+'"]').hide().fadeIn(fadeInTime);    
+// ----------------------------------- zarządzanie rekordami ----------------------------------- //
+// --------- formularz dodawania: tworzenie formularza, anulowanie i dodawanie rekordu --------- //
+function clickCreateRowButtons() {   // kliknięcie przycisku dodawania
+    $('#showCreateRow').click(function(){       // tworzenie formularza dodawania
+        $('tr#createRow').remove();
+        var communique = "Błąd tworzenia wiersza z formularzem dodawania szkoły.";
+        var CreateRow = new CreateRowService(NUMBER_OF_FIELDS, TABLE_NAME, communique, INPUT_NAME);
+        $(TABLE_NAME).animate({width: '95%'}, 500);
+        $.when( $(this).slideUp(SLIDE_UP) ).then(function() { CreateRow.show(ROUTE_NAME); });
     });
-}
 
-// ----------------------------------- zarządzanie szkołami ------------------------------------ //
-function showCreateRowButtonClick() {
-    $('#showCreateRow').click(function(){
-        $('table#schools').animate({width: '80%'}, 500);
-        $.when( $(this).fadeOut(fadeOutTime) ).then(function() { showCreateRow(); });
+    $(TABLE_NAME).delegate('button#cancelAdd', 'click', function() {    // kliknięcie przycisku "anuluj"
+        $.when( $('#createRow').fadeOut(FADE_OUT) ).then(function() {
+            $('#createRow').remove();
+            $('#showCreateRow').slideDown(SLIDE_DOWN);
+        });
     });
-}
 
-function showCreateRow() {
-    $.ajax({
-        method: "GET",
-        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-        url: "http://localhost/school/szkola/create",
-        success: function(result) { showCreateRowSuccess(result); },
-        error: function() { showCreateRowError(); },
-    });
-}
-
-function showCreateRowError() {
-    var error = '<tr><td colspan="4" class="error">Błąd tworzenia wiersza z formularzem dodawania szkoły.</td></tr>';
-    $('table#schools tr.create').after(error);
-    $('td.error').hide().fadeIn(fadeInTime);
-}
-
-function showCreateRowSuccess(result) {
-    $('table#schools tr.create').before(result);
-    $('#createRow').hide().fadeIn(fadeInTime);
-    $('input[name="name"]').focus();
-}
-
-function createRowClickButtons() {     // ustawienie instrukcji po kliknięciu anulowania lub potwierdzenia dodawania szkoły
-    $('table#schools').delegate('#cancelAdd', 'click', function() { createRowClickCancel(); });
-    $('table#schools').delegate('#add', 'click', function() { createRowClickAdd(); });
-}
-
-function createRowClickCancel() {
-    $.when( $('#createRow').fadeOut(fadeOutTime) ).then(function() {
-        $('#createRow').remove();
-        $('#showCreateRow').fadeIn(fadeInTime);
-    });
-}
-
-function createRowClickAdd() {
-    $.when( $('#createRow').fadeOut(fadeOutTime) ).then(function() {
-        $('#showCreateRow').fadeIn(fadeInTime);
-        add();
-    });
-}
-
-function add() {   // zapisanie nauczyciela w bazie danych
-    var name    = $('#createRow input[name="name"]').val();
-    var id_OKE  = $('#createRow input[name="id_OKE"]').val();
-
-    $.ajax({
-        method: "POST",
-        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-        url: "http://localhost/school/szkola",
-        data: { name: name, id_OKE: id_OKE },
-        success: function(id) { addSuccess(id); },
-        error: function() { addError(); },
-    });
-}
-
-function addError() {
-    var error = '<tr><td colspan="5" class="error">Błąd dodawania szkoły.</td></tr>';
-    $('table#schools tr.create').before(error);
-    $('td.error').hide().fadeIn(fadeInTime);
-}
-
-function addSuccess() {
-    var lp = parseInt( $('#countSchools').val() ) + 1;
-    refreshRow(id, lp);
-    $('#countTeachers').html(lp);
-}
-
-function editButtonClick() {     // kliknięcie przycisku modyfikowania szkoły
-    $('#schools').delegate('button.edit', 'click', function() {
-        var id = $(this).data('school_id');
-        $.ajax({
-            type: "GET",
-            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            url: "http://localhost/school/szkola/"+id+"/edit",
-            data: { id: id },
-            success: function(result) { editButtonClickSuccess(id, result); },
-            error: function() { editButtonClickError(id); },
+    $(TABLE_NAME).delegate('button#add', 'click', function() {          // kliknięcie przycisku "dodaj"
+        $.when( $('#createRow').fadeOut(FADE_OUT) ).then(function() {
+            $('#showCreateRow').slideDown(SLIDE_DOWN);
+            add();
         });
     });
 }
 
-function editButtonClickError(id) {
-    var error = '<tr><td colspan="4" class="error">Nie mogę utworzyć formularza do zmiany danych.</td></tr>';
-    $('tr[data-school_id="'+id+'"]').before(error);
-    $('td.error').hide().fadeIn(fadeInTime);
+// ----------------------------- wstawienie rekordu do bazy danych ----------------------------- //
+function add() {
+    var name    = $('#createRow input[name="name"]').val();
+    var id_OKE  = $('#createRow input[name="id_OKE"]').val();
+    var lp = parseInt( $('#countSchools').val() ) + 1;
+
+    $.ajax({
+        method: "POST",
+        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+        url: "http://localhost/school/" +ROUTE_NAME,
+        data: { name: name, id_OKE: id_OKE },
+        success: function(id) { refreshRow(id, lp, "add", true); },
+        error: function() { refreshRow(0, lp, "add", false); },
+    });
 }
 
-function editButtonClickSuccess(id, result) {
-    $.when( $('tr[data-school_id="'+id+'"]').fadeOut(fadeOutTime) ).then(function() {
-        var lp = $('tr[data-school_id="'+id+'"]').children(":first").html();
-        $('tr[data-school_id="'+id+'"]').replaceWith(result);
-        $('tr.editRow[data-school_id="'+id+'"]').hide().fadeIn(fadeInTime);
-        $('input[name="name"]').focus();
-        editRowClickButtons(lp);
+// -------- formularz modyfikowania: tworzenie formularza, anulowanie i zmiana rekordu --------- //
+function clicEditRowButtons() {
+    $(TABLE_NAME).delegate('button.edit', 'click', function() {     // tworzenie formularza modyfikowania
+        var communique = "Nie mogę utworzyć formularza do zmiany danych szkoły.";
+        var EditRow = new EditRowService(NUMBER_OF_FIELDS, DATA_NAME, communique, INPUT_NAME);
+        var id = $(this).data(DATA_NAME);
+        var lp = $('tr[data-' +DATA_NAME+ '="'+id+'"]').children(":first").html();
+        EditRow.show(ROUTE_NAME, id, lp);
     });
 
+    $(TABLE_NAME).delegate('button.cancelUpdate', 'click', function() { // kliknięcie przycisku "anuluj"
+        var id = $(this).data(DATA_NAME);
+        var lp = $('var.lp').html();
+        $.when( $('.editRow[data-' +DATA_NAME+ '=' +id+ ']').fadeOut(FADE_OUT) ).then( function() { refreshRow(id, lp, "cancelUpdate", true); });
+    });
+
+    $(TABLE_NAME).delegate('button.update', 'click', function() {       // kliknięcie przycisku "zapisz"
+        var id = $(this).data(DATA_NAME);
+        var lp = $('var.lp').html();
+        $.when( $('.editRow[data-' +DATA_NAME+ '=' +id+ ']').fadeOut(FADE_OUT) ).then( function() { update(id, lp); });
+    });
 }
 
-function editRowClickButtons(lp) {     // ustawienie instrukcji po kliknięciu anulowania lub potwierdzenia modyfikowania szkoły
-    $('.cancelUpdate').click(function() { editRowClickCancel( $(this).data('school_id'), lp ); });
-    $('.update').click(function()       { editRowClickUpdate( $(this).data('school_id'), lp ); });
-}
-
-function editRowClickCancel(id, lp) {
-    $.when( $('.editRow[data-school_id="'+id+'"]').fadeOut(fadeOutTime) ).then(function() { refreshRow(id, lp, false); });
-}
-
-function editRowClickUpdate(id, lp) {
-    $.when( $('.editRow[data-school_id='+id+']').fadeOut(fadeOutTime) ).then( function() { update(id, lp); });
-}
-
-function update(id, lp) {   // zapisanie zmian w bazie danych
+// --------------------------------- zapisywanie zmian rekordu --------------------------------- //
+function update(id, lp) {
     var name    = $('tr[data-school_id="'+id+'"] input[name="name"]').val();
     var id_OKE  = $('tr[data-school_id="'+id+'"] input[name="id_OKE"]').val();
 
     $.ajax({
         method: "PUT",
         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-        url: "http://localhost/school/szkola/"+id,
+        url: "http://localhost/school/" +ROUTE_NAME+ "/"+id,
         data: { id: id, name: name, id_OKE: id_OKE },
-        success: function() { refreshRow(id, lp, false); },
-        error:   function() { updateError(id, lp); },
+        success: function() { refreshRow(id, lp, "update", true); },
+        error:   function() { refreshRow(id, lp, "update", false); },
     });
 }
 
-function updateError(id, lp) {
-    $.when( refreshRow(id, lp, false) ).then(function() {
-        var error = '<tr><td colspan="4" class="error">Błąd w czasie zapisywania informacji.</td></tr>';
-        $('tr[data-school_id='+id+']').before(error);
-        $('td.error').hide().fadeIn(fadeInTime);
-    });
-}
-
-function destroyButtonClick() {  // usunięcie szkoły (z bazy danych)
-    $('table#schools').delegate('button.destroy', 'click', function() {
-        var id = $(this).data('school_id');
+// ------------------------------------- usuwanie rekordu -------------------------------------- //
+function clickDestroyButton() {
+    $(TABLE_NAME).delegate('button.destroy', 'click', function() {
+        var id = $(this).data(DATA_NAME);
         $.ajax({
             type: "DELETE",
             headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            url: "http://localhost/school/szkola/" + id,
-            success: function() { destroySuccess(id); },
-            error:   function() { destroyError(id); }
+            url: "http://localhost/school/" +ROUTE_NAME+ "/" + id,
+            success: function() { refreshRow(id, 0, "destroy", true); },
+            error:   function() { refreshRow(id, 0, "destroy", false); }
         });
-        return false;
     });
-}
-
-function destroyError(id) {
-    var error = '<tr data-school_id="'+id+'"><td colspan="4" class="error">Nie mogę usunąć szkoły. Prawdopodobnie istnieją powiązane rekordy.</td></tr>';
-    $('tr[data-school_id='+id+']').before(error);
-    $('td.error').hide().fadeIn(fadeInTime);
-}
-
-function destroySuccess(id) {
-    $.when( $('tr[data-school_id='+id+']').fadeOut(fadeOutTime) ).then( function() {
-        $('tr[data-school_id='+id+']').remove();
-     });
 }
 
 // ---------------------- wydarzenia wywoływane po załadowaniu dokumnetu ----------------------- //
 $(document).ready(function() {
-    showCreateRowButtonClick();
-    createRowClickButtons();
-    editButtonClick();
-    destroyButtonClick();
+    clickCreateRowButtons();
+    clicEditRowButtons();
+    clickDestroyButton();
 });
